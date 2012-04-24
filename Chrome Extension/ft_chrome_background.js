5502719a93806a2c28b9308f9a2d79c150eec6ac
Matthew Monahan <monahan.matthewjames@gmail.com>
2012-04-24 00:19:56 -0400

    Test commit

---
+++ Chrome Extension/ft_chrome_background.js
@@ -0,0 +1,33 @@
+/**
+ * @author Matthew Monahan
+ */
+	
+chrome.browserAction.onClicked.addListener(function(tab) {
+/*	
+	var popups = chrome.extension.getViews({type: "popup"});
+	if (popups.length != 0) {
+		var popup = popups[0];
+		popup.setdiv(tab.url);
+	};
+	
+
+  var viewTabUrl = chrome.extension.getURL('chrome_popup.html');
+  var urlSend = tab.url;
+
+  // Look through all the pages in this extension to find one we can use.
+  var views = chrome.extension.getViews();
+  for (var i = 0; i < views.length; i++) {
+    var view = views[i];
+
+    // If this view has the right URL and hasn't been used yet...
+    if (view.location.href == viewTabUrl) {
+
+      // ...call one of its functions and set a property.
+      view.setdiv(urlSend);
+      break; // we're done
+    }
+  }
+  
+*/
+
+});
---
+++ Chrome Extension/ft_chrome_contentscript.js
@@ -0,0 +1,3 @@
+/**
+ * @author Matthew Monahan
+ */
